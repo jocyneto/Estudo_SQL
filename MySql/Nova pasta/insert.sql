afaf1b6f50bfde09e0b9a7a97c971f2fe916adf4
jocyneto <58772980+jocyneto@users.noreply.github.com>
2021-02-04 08:26:59 -0300

    Create insert.sql

---
+++ MySql/Nova pasta/insert.sql
@@ -0,0 +1,92 @@
+-- CPF:1471156710 NOME:Érica Carvalho ENDEREÇO:R. Iriquitia BAIRRO:Jardins CIDADE:São Paulo ESTADO:SP CEP:80012212 DATA DE NASCIMENTO:01/09/1990 IDADE:27 SEXO: F LIMITE DE CRÉDITO:170000 VOLUME DE COMPRA:24500 PRIMEIRA COMPRA: Não
+
+INSERT INTO clientes 
+(	cpf
+	, nome
+    , endereco
+    , bairro
+    , cidade
+    , estado
+    , cep
+    , data_nascimento
+    , idade
+    , sexo
+    , limite_credito
+    , volume_compra
+    , primeira_compra)
+values
+(	'1471156710'
+	, 'Érica Carvalho'
+    , 'R. Iriquitia'
+    , 'Jardins'
+    , 'São Paulo'
+    , 'SP'
+    , '80012212'
+    , '1990-09-01'
+    , 27
+    , 'F'
+    , 170000
+    , 24500
+    , 0);
+    
+    
+-- CPF:19290992743 NOME:Fernando Cavalcante ENDEREÇO:R. Dois de Fevereiro BAIRRO:Água Santa CIDADE:Rio de Janeiro ESTADO:RJ CEP:22000000 DATA DE NASCIMENTO:12/02/2000 IDADE:18 SEXO:M LIMITE DE CRÉDITO:100000 VOLUME DE COMPRA:20000 PRIMEIRA COMPRA:Sim
+
+INSERT INTO clientes 
+(	cpf
+	, nome
+    , endereco
+    , bairro
+    , cidade
+    , estado
+    , cep
+    , data_nascimento
+    , idade
+    , sexo
+    , limite_credito
+    , volume_compra
+    , primeira_compra)
+values
+(	'19290992743'
+	, 'Fernando Cavalcante'
+    , 'R. Dois de Fevereiro'
+    , 'Água Santa'
+    , 'Rio de Janeiro'
+    , 'RJ'
+    , '22000000'
+    , '2000-02-12'
+    , 18
+    , 'M'
+    , 100000
+    , 20000
+    , 1);
+
+-- CPF: 2600586709 NOME: César Teixeira ENDEREÇO: Rua Conde de Bonfim BAIRRO: Tijuca CIDADE: Rio de Janeiro ESTADO: RJ CEP: 22020001 DATA DE NASCIMENTO: 12/03/2000 IDADE: 18 SEXO: M LIMITE DE CREDITO: 120000 VOLUME DE COMPRA: 22000 PRIMEIRA COMPRA: Não
+INSERT INTO clientes 
+(	cpf
+	, nome
+    , endereco
+    , bairro
+    , cidade
+    , estado
+    , cep
+    , data_nascimento
+    , idade
+    , sexo
+    , limite_credito
+    , volume_compra
+    , primeira_compra)
+values
+(	'2600586709'
+	,'César Teixeira'
+    ,'Rua Conde de Bonfim'
+    ,'Tijuca'
+    ,'Rio de Janeiro'
+    ,'RJ'
+    ,'22020001'
+    ,'2000-03-12'
+    ,18
+    ,'M'
+    ,120000
+    ,22000 
+    ,0);
